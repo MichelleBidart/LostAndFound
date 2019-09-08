8c95bf2fc35f948689fcd74c8c4331628b258f54
mbidart <mbidart@BALT9-DEV-1065>
2019-09-08 16:46:37 -0300

    add app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+/*const express = require('express');
+
+const app = express();
+
+const users = [
+    { mail: 'bidart.michelle@gmail.com', password: '1234' },
+    { mail: 'martinbrude@gmail.com', password: '45678' },
+];
+
+app.get('/users', (req, res) => {
+
+    res.send(users);
+
+});*/
+
+app.listen(3000, () => console.log('init.....'));
